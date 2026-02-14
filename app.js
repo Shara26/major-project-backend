6048a3d2b1ed69d43ba51c2dc20648970d3e2d88
Shara26 <sharatadas26@gmail.com>
2026-02-14 23:32:59 +0530

    Configure EJS templating and render dynamic listings from MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,11 +18,28 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-app.get("/testListing", async(req, res) => {
+//Index Route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+    });
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+/*app.get("/testListing", async(req, res) => {
     let sampleListing = new Listing({
         title: "My New Villa",
         description: "By the beach",
@@ -33,7 +51,7 @@ app.get("/testListing", async(req, res) => {
     await sampleListing.save();
     console.log("sample was saved");
     res.send("successful testing");
-});
+}); */
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
