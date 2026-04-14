8bcbab60b95028ce7b250a79ad2e34876ebe7380
Shara26 <sharatadas26@gmail.com>
2026-04-14 18:52:59 +0530

    Changes

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,17 +5,20 @@ const Review = require("../models/review");
 module.exports.createReview = async(req, res) => {
     console.log(req.params.id);
     let listing = await Listing.findById(req.params.id);
+
     let newReview = new Review(req.body.review);
     newReview.author = req.user._id;
+
     console.log(newReview);
+
     listing.reviews.push(newReview);
     
     await newReview.save();
     await listing.save();
-    req.flash("success", "New Review Created!");
-    router.post("/",isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
-};
 
+    req.flash("success", "New Review Created!");
+    res.redirect(`/listings/${listing._id}`);   
+};
 
 module.exports.destroyReview = async (req, res) => {
      let{ id, reviewId } = req.params;
